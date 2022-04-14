5d75c46ff54be4dd8c630eae2457e94bbc86eb87
Blanca <blancacano2.71828@gmail.com>
2022-04-14 12:15:13 +0200

    Elimino líneas innecesarias nodo simple #1 #2

--- practica_1/src/src/tracks/singlePlayer/evaluacion/src_CANO_CAMARERO_BLANCA/NodoSimple.java
+++ practica_1/src/src/tracks/singlePlayer/evaluacion/src_CANO_CAMARERO_BLANCA/NodoSimple.java
@@ -3,18 +3,15 @@ package tracks.singlePlayer.evaluacion.src_CANO_CAMARERO_BLANCA;
 import java.util.LinkedList;
 import java.util.Queue;
 import ontology.Types;
-import tools.Vector2d;
 
 /**
  * Encapsula la información elemental de un nodo 
  * usada en los problemas de BFS
  */
 public class NodoSimple {
-    //Vector2d coordenadas; 
     int x,y;
     Queue<Types.ACTIONS> historialPasos;
-
-
+    
     /** Constructor para nodo inicial 
      * coordenadas iniciales x e y 
     */
